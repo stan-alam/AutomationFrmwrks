dca4e5a12545b3601d57b1e6bd51dd8e82db0660
stan.alam <stan.alam@gmail.com>
2022-06-22 22:03:25 -0400

    added container for driver from which the instance of driver can be created - no need for ThreadLocal

--- Java/framework/utils/DriverContainer.java
+++ Java/framework/utils/DriverContainer.java
@@ -2,3 +2,11 @@ public class DriverContainter {
 
   private static final ThreadLocal<WebDriver> DRIVER_THREAD = new ThreadLocal <>(); //since both nUnit the one that you were using in the C# atomo project, and TestNG are multithreaded/threadsave
 }
+
+public static WebDriver getDriverThread() {
+  return DRIVER_THREAD.get();
+}
+
+public static void setDriverThread(final WebDriver webDriverValue) {
+  DRIVER_THREAD.set(webDriverValue);
+}
