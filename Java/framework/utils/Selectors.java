53f63723c7cd70a3feca085a6265eac0ad5fb5c0
stan.alam <stan.alam@gmail.com>
2022-06-25 00:02:15 -0400

    added static selectors

---
+++ Java/framework/utils/Selectors.java
@@ -0,0 +1,7 @@
+public class Selectors {
+ // put all login in one UI.Login_selectors ? SideNav wit its own selectors
+  private static final By LOGIN_USERNAME = By.id("QA_ID_InputText_login"); //use selenide JQuery
+
+  private static final By LOGIN_PASSWORD = By.id("QA_ID_InputText_password");
+  
+}
